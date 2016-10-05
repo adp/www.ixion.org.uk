fe5f0f99ae511b3121919dc0b94f722ddd8ee4f2
Andrew Pounce <adp@ntohs.co.uk>
2016-10-05 15:34:17 +0100

    Initial commit - content recovered from wayback machine..

---
+++ cgi-bin/calendar/calendar.pl
@@ -0,0 +1,152 @@
+<HTML>
+<HEAD>
+<TITLE>Ixion Events - August 2008</TITLE>
+<link rel=stylesheet href="/Styles/Ixion.css" type="text/css">
+<SCRIPT LANGUAGE="JavaScript">
+function viewday (yr,mm,dd) {
+  window.open('/cgi-bin/calendar/calendar.pl?ACTION=VIEWDAY&Year=' +yr+ '&Month=' +mm+ '&Date=' +dd + '&config=calendar.cfg','VIEWDAY','scrollbars,resizable,height=400,width=450');
+}
+</SCRIPT>
+</HEAD>
+<BODY BGCOLOR="#FFFFFF">
+<TABLE BORDER=0 WIDTH=100%>
+<TR>
+<TD ALIGN=LEFT><FONT SIZE=7 COLOR="#008000"><A HREF="http://www.ixion.org.uk/"><IMG SRC="/images/iximetals.gif" WIDTH=118 HEIGHT=40 ALT="Ixion"></A> Events Calendar</FONT></TD>
+<TD ALIGN=RIGHT><FONT SIZE=7 COLOR="#008000">August&nbsp;2008</FONT></TD>
+</TR>
+</TABLE>
+<TABLE BORDER=0 WIDTH=100%>
+<TR>
+<TD WIDTH=20% ALIGN=LEFT><A HREF="http://www.ixion.org.uk/calendar/">Front Page</A></TD>
+<TD WIDTH=60% ALIGN=CENTER><form action='/cgi-bin/calendar/calendar.pl' method=post><input type='hidden' name='config' value='calendar.cfg'><select name='Month' size=1><option value='1' >January</option><option value='2' >February</option><option value='3' >March</option><option value='4' >April</option><option value='5' >May</option><option value='6' >June</option><option value='7' >July</option><option value='8'  SELECTED >August</option><option value='9' >September</option><option value='10' >October</option><option value='11' >November</option><option value='12' >December</option></select><select name='Year' size=1><option value='2007' >2007
+<option value='2008'  SELECTED >2008
+<option value='2009' >2009
+<option value='2010' >2010
+</select><input type="submit" CLASS="button" value="Go"><br></form></TD>
+<TD WIDTH=20% ALIGN=RIGHT><A HREF="#add">Add Event Form</A></TD>
+</TR>
+</TABLE>
+<TABLE WIDTH=100% BORDER=1>
+</TR>
+<TR>
+<TH WIDTH=14%>Monday</TH>
+<TH WIDTH=14%>Tuesday</TH>
+<TH WIDTH=14%>Wednesday</TH>
+<TH WIDTH=14%>Thursday</TH>
+<TH WIDTH=14%>Friday</TH>
+<TH WIDTH=15%>Saturday</TH>
+<TH WIDTH=15%>Sunday</TH>
+</TR>
+<TR><TD COLSPAN=4 >&nbsp;</TD>
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,1);">1</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,2);">2</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,3);">3</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+</TR><TR>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,4);">4</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,5);">5</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,6);">6</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,7);">7</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,8);">8</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,9);">9</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,10);">10</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+</TR><TR>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,11);">11</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,12);">12</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,13);">13</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,14);">14</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,15);">15</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,16);">16</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,17);">17</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+</TR><TR>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,18);">18</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,19);">19</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,20);">20</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,21);">21</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,22);">22</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,23);">23</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,24);">24</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+</TR><TR>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,25);">25</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,26);">26</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,27);">27</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=yellow><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,28);">28</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,29);">29</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,30);">30</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+<TD VALIGN=TOP BGCOLOR=white><FONT SIZE=+1><B><A HREF="javascript:viewday(2008,8,31);">31</A></B></FONT><FONT SIZE="-1"><BR><br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</FONT></TD>
+
+</TABLE>
+<CENTER>
+<P>
+<FORM ACTION="/cgi-bin/calendar/calendar.pl" METHOD=POST>
+<INPUT TYPE="HIDDEN" NAME="ACTION" VALUE="DO_ADD">
+<INPUT TYPE="HIDDEN" NAME="config" VALUE="calendar.cfg">
+<A NAME="add"></A>
+<FONT SIZE=+3 COLOR="#008000">Add Event</FONT>
+<TABLE BORDER=0>
+<TR>
+<TD ALIGN=CENTER><B>Event Date:&nbsp;</B>
+<INPUT NAME="Date" SIZE=2 MAXLENGTH=2 VALUE=""> / 
+<INPUT NAME="Month" SIZE=2 MAXLENGTH=2 VALUE=""> / 
+<INPUT NAME="Year" SIZE=4 MAXLENGTH=4 VALUE="">
+</TD>
+<TD ALIGN=CENTER>
+<INPUT TYPE="checkbox" NAME="annual" VALUE="1" onClick="if(form.annual.checked == true){form.Year.value='0000'}else{form.Year.value='2008'}"> <B>Every Year</B>
+</TD>
+</TR>
+<TD ALIGN=CENTER COLSPAN=2><B>Event Name</B> (HTML tags permitted)<BR>
+<INPUT NAME="heading" SIZE=60 VALUE="">
+</TD>
+</TR>
+<TR>
+<TD ALIGN=CENTER COLSPAN=2><B>Event Description</B><BR>
+<TEXTAREA NAME="description" COLS=60 ROWS=5 WRAP=SOFT></TEXTAREA>
+</TD>
+</TR>
+<TR>
+<TD ALIGN=CENTER COLSPAN=2>
+<INPUT TYPE="RESET" VALUE="Cancel">&nbsp;&nbsp
+<INPUT TYPE="submit" VALUE="Save">
+</TD>
+</TR>
+</TABLE>
+</CENTER>
+<TABLE BORDER=0 WIDTH=100%><TR><TD ALIGN=RIGHT><FONT SIZE="-1"><I>Calendar program by <A HREF="http://mkruse.netexpress.net/">Matt Kruse</A></I></FONT>
+</TD>
+</TR>
+</TABLE>
+</BODY>
+</HTML>
